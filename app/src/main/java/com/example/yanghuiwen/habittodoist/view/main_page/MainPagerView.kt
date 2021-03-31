f2fe55fc6640eb9d623ebcc0f3ea77e21c4ff6de
kikiouo <kikiouo201@gmail.com>
2021-04-01 00:16:43 +0800

    Modify the interface in main page

--- app/src/main/java/com/example/yanghuiwen/habittodoist/view/main_page/MainPagerView.kt
+++ app/src/main/java/com/example/yanghuiwen/habittodoist/view/main_page/MainPagerView.kt
@@ -2,121 +2,151 @@ package com.example.yanghuiwen.habittodoist.view.main_page
 
 import android.content.Context
 import android.os.Build
-import android.text.SpannableString
-import android.text.style.UnderlineSpan
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.annotation.RequiresApi
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
-import com.example.yanghuiwen.habittodoist.AllItemData
-import com.example.yanghuiwen.habittodoist.ItemDate
 import com.example.yanghuiwen.habittodoist.R
-import com.example.yanghuiwen.habittodoist.view.item_sample.SingleItem
-import com.example.yanghuiwen.habittodoist.view.week_viewpager.WeekPageView
-import com.example.yanghuiwen.habittodoist.view.week_viewpager.WeekPagerAdapter
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
+import com.google.android.material.tabs.TabLayout
+import java.util.ArrayList
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainPageView(context: Context) : RelativeLayout(context){
     val view = LayoutInflater.from(context).inflate(R.layout.main_page, null)
-//    val weeks =intArrayOf(R.id.week7, R.id.week1, R.id.week2, R.id.week3, R.id.week4, R.id.week5, R.id.week6)
-//    var startWeek = startDate.dayOfWeek.getValue()
-//    val weekSeven = startDate.minusDays(startWeek.toLong())
-//    var currentDateIndex = startWeek
-//    var currentOnDateChange = onDateChange
-
-    var todayList: SingleItem<String>?  = null
-    var habitList: SingleItem<String>?  = null
-    var scheduleList: SingleItem<String>? =null
-    private lateinit var pageList: MutableList<WeekPageView>
-    var habit_RecyclerView:RecyclerView? = null
+    private lateinit var mainPageList: MutableList<RelativeLayout>
     init {
 
+        mainPageList = ArrayList()
+        mainPageList.add(IntervalDayView(context))// 年
+        mainPageList.add(IntervalDayView(context))// 月
+        mainPageList.add(IntervalDayView(context))// 週
+        mainPageList.add(DayView(context))// 日
 
+        var chooseSortItem = 0
+//        val sortSingleItem =arrayOf("重要性","單項","專案")
+//        val sort= view.findViewById<Button>(R.id.sort)
 
-        var nowDate = LocalDateTime.now()
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        val currentDate = nowDate.format(formatter)
-        //Log.i("MainPagerView","currentDate="+currentDate)
-        initAllToDoList(currentDate)
-
-        //set now date
-        val year = view.findViewById<TextView>(R.id.year)
-        year.setText(nowDate.year.toString())
-        val month = view.findViewById<TextView>(R.id.month)
-        month.setText((nowDate.month.value).toString())
-        val day = view.findViewById<TextView>(R.id.day)
-        day.setText(nowDate.dayOfMonth.toString())
-
-
-        initWeekViewpage()
-
-        addView(view)
-    }
-
-    fun chooseThisPage(){
-
-    }
-    fun refreshView() {
-
-    }
-
-    fun initAllToDoList(currentDate :String) {
-        AllItemData.currentDate = currentDate
-        scheduleList = SingleItem(context,AllItemData.getDateActivity(),"activity")
-        val scheduleLayoutManager = LinearLayoutManager(context);
-        scheduleLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        val schedule_RecyclerView = view.findViewById<View>(R.id.activityList) as RecyclerView
-        schedule_RecyclerView.layoutManager = scheduleLayoutManager
-        schedule_RecyclerView.adapter = scheduleList
-
-        todayList = SingleItem(context,AllItemData.getDateToDayToDo(),"todayToDo")
-        val layoutManager = LinearLayoutManager(context)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        val mRecyclerView = view.findViewById<View>(R.id.todayList) as RecyclerView
-        mRecyclerView.layoutManager = layoutManager
-        mRecyclerView.adapter = todayList
-
-        habitList = SingleItem(context,AllItemData.getDateHabitToDo(),"habitToDo")
-        val habitLayoutManager = LinearLayoutManager(context)
-        habitLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        habit_RecyclerView = view.findViewById<View>(R.id.habitList) as RecyclerView
-        habit_RecyclerView?.layoutManager = habitLayoutManager
-        habit_RecyclerView?.adapter = habitList
-    }
-
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun initWeekViewpage() {
-
-        val current = LocalDateTime.now()
-
-        AllItemData.currentWeekIndex = current.dayOfWeek.getValue()%7
-        pageList = ArrayList()
-
-        pageList.add(WeekPageView(context, current.minusDays(21),this::initAllToDoList))
-        pageList.add(WeekPageView(context, current.minusDays(14),this::initAllToDoList))
-        pageList.add(WeekPageView(context, current.minusDays(7),this::initAllToDoList))
-        pageList.add(WeekPageView(context, current,this::initAllToDoList))
-        pageList.add(WeekPageView(context, current.plusDays(7),this::initAllToDoList))
-        pageList.add(WeekPageView(context, current.plusDays(14),this::initAllToDoList))
-        pageList.add(WeekPageView(context, current.plusDays(21),this::initAllToDoList))
-
-
+        val tabs= view.findViewById<TabLayout>(R.id.tabLayout)
         val page = view.findViewById<ViewPager>(R.id.pager)
-        val weekPagerAdapter = WeekPagerAdapter(pageList)
+        val mainPagerAdapter = PagerAdapter(mainPageList)
+       // val isDisplays =view.findViewById<ImageButton>(R.id.isDisplay)
+
+        val notDate = mainPageList[0] as IntervalDayView
+//        sort.setOnClickListener {
+//            chooseSortItem++
+//            if(chooseSortItem == 3){
+//                chooseSortItem = 0
+//            }
+//
+//            Log.i("OtherToDoListPagerView","chooseSortItem${chooseSortItem}")
+//            sort.text = sortSingleItem[chooseSortItem]
+//            notDate.chooseSortPage(sortSingleItem[chooseSortItem])
+//        }
 
 
+        // tab 的換頁
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Log.i("OtherToDoListPagerView","p0=${tab?.text}")
+                if(tab != null){
+                    when(tab.text){
+                        "年" ->{
+                            val singleItemView = mainPageList[0] as IntervalDayView
+//                            singleItemView.chooseSortPage(tab.text.toString())
+//                            var isDisplay =true
+//                            isDisplays.setOnClickListener{
+//                                if(isDisplay){
+//                                    singleItemView.isDisplayOff()
+//                                }else{
+//                                    singleItemView.isDisplayOn()
+//                                }
+//                                isDisplay = !isDisplay
+//                            }
+                        }
+                        "月" -> {
+                            val singleItemView = mainPageList[1] as IntervalDayView
+//                            singleItemView.chooseSortPage(tab.text.toString())
+//                            var isDisplay =true
+//                            isDisplays.setOnClickListener{
+//                                if(isDisplay){
+//                                    singleItemView.isDisplayOff()
+//                                }else{
+//                                    singleItemView.isDisplayOn()
+//                                }
+//                                isDisplay = !isDisplay
+//                            }
+//                            sort.setOnClickListener {
+//                                chooseSortItem++
+//                                if(chooseSortItem == 3){
+//                                    chooseSortItem = 0
+//                                }
+//
+//                               // Log.i("OtherToDoListPagerView","chooseSortItem${chooseSortItem}")
+//                           //     sort.text = sortSingleItem[chooseSortItem]
+//                                singleitemView.chooseSortPage(sortSingleItem[chooseSortItem])
+//                            }
+                        }
+                        "週" -> {
+                            val singleItemView = mainPageList[2] as IntervalDayView
+//                            singleItemView.chooseSortPage(tab.text.toString())
+//                            var isDisplay =true
+//                            isDisplays.setOnClickListener{
+//                                if(isDisplay){
+//                                    singleItemView.isDisplayOff()
+//                                }else{
+//                                    singleItemView.isDisplayOn()
+//                                }
+//                                isDisplay = !isDisplay
+//                            }
+//                            sort.setOnClickListener {
+//                                chooseSortItem++
+//                                if(chooseSortItem == 3){
+//                                    chooseSortItem = 0
+//                                }
+//
+//                                // Log.i("OtherToDoListPagerView","chooseSortItem${chooseSortItem}")
+//                                sort.text = sortSingleItem[chooseSortItem]
+//                                singleitemView.chooseSortPage(sortSingleItem[chooseSortItem])
+//                            }
+
+                        }
+                        "日" -> {
+                            val habitItemView = mainPageList[3] as DayView
+//                            habitItemView.chooseSortPage(tab.text.toString())
+//                            var isDisplay =true
+//                            isDisplays.setOnClickListener{
+//                                if(isDisplay){
+//                                    habitItemView.isDisplayOff()
+//                                }else{
+//                                    habitItemView.isDisplayOn()
+//                                }
+//                                isDisplay = !isDisplay
+//                            }
+//                            sort.setOnClickListener {
+//                                chooseSortItem++
+//                                if(chooseSortItem == 3){
+//                                    chooseSortItem = 0
+//                                }
+//
+//                                // Log.i("OtherToDoListPagerView","chooseSortItem${chooseSortItem}")
+//                                sort.text = sortSingleItem[chooseSortItem]
+//                                habitItemView.chooseSortPage(sortSingleItem[chooseSortItem])
+//                            }
+                        }
+                    }
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+        // viewpager 的換頁
         val listener = object: ViewPager.OnPageChangeListener{
             override fun onPageScrollStateChanged(p0: Int) {}
 
@@ -125,87 +155,40 @@ class MainPageView(context: Context) : RelativeLayout(context){
                 //textView.setText("$pageNow / ${list.size}")
             }
             override fun onPageSelected(p0: Int) {
-                Log.i("kiki","p0=${p0}")
-                pageList[p0].chooseThisPage()
+
             }
         }
 
-        page.adapter = weekPagerAdapter
+        page.adapter = mainPagerAdapter
+        page.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.setOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(page))
         page.addOnPageChangeListener(listener)
 
-        page.setCurrentItem(3 , false);
+
+        page.setCurrentItem(3 , false)
+        addView(view)
+    }
+
+    fun chooseThisPage(){
+//        for (i in 0..2){
+//            val notDate = mainPageList[i] as ProjectSortSingleitemView
+//            notDate.chooseThisPage()
+//        }
+        val notDate0 = mainPageList[0] as IntervalDayView
+        notDate0.chooseThisPage()
+        val notDate1 = mainPageList[1] as IntervalDayView
+        notDate1.chooseThisPage()
+        val notDate2 = mainPageList[2] as IntervalDayView
+        notDate2.chooseThisPage()
+        val notDate3 = mainPageList[3] as DayView
+        notDate3.chooseThisPage()
+
+    }
+    fun refreshView() {
+
     }
 
 
-
-
-
-//     fun onArticleSelected(position: AllItemData) {
-//        Log.i("kiki","habitToDo="+position.habitToDo)
-//        habitList = SingleItem(position.habitToDo,"habitToDo")
-//
-//        habit_RecyclerView?.adapter = habitList
-//    }
-
-
-
-
-
-
-
-    inner class ScheduleItem<T>(data: ArrayList<ItemDate>) : RecyclerView.Adapter<ScheduleItem<T>.ViewHolder>() {
-        var todoData: ArrayList<ItemDate> = ArrayList()
-
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var startTimeTextView: TextView
-            var endTimeTextView: TextView
-            var infoText:TextView
-            init {
-                startTimeTextView = itemView.findViewById<View>(R.id.startTime) as TextView
-                endTimeTextView = itemView.findViewById<View>(R.id.endTime) as TextView
-                infoText = itemView.findViewById<View>(R.id.info_text) as TextView
-            }
-
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val v = LayoutInflater.from(parent.context).inflate(R.layout.schedule, parent, false)
-            return ViewHolder(v)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val startTime = if(position<10) "0${position}:00" else "${position}:00"
-            val endTime = if(position+1<10) "0${position+1}:00" else "${position+1}:00"
-
-            holder.infoText.setText("")
-            for (i in 0..todoData.size-1){
-                val (hour,min)  = todoData[i].startTime.split(":")
-                if(hour.toInt()==position){
-                    if(position<10){
-                        holder.infoText.setText(todoData[i].name)
-                    }else{
-                        holder.infoText.setText(todoData[i].name)
-                    }
-                }
-            }
-
-
-            holder.startTimeTextView.text = startTime
-            holder.endTimeTextView.text = endTime
-            holder.startTimeTextView.setOnLongClickListener { false }
-            holder.endTimeTextView.setOnLongClickListener { false }
-        }
-
-
-
-        override fun getItemCount(): Int {
-            return 24
-        }
-
-        init {
-            todoData = data
-        }
-    }
 
 
 
