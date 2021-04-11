65c6e9215533fd20c2aed99b787264799dfae981
kikiouo <kikiouo201@gmail.com>
2021-04-11 21:15:16 +0800

    Add the project result interface

---
+++ app/src/main/java/com/example/yanghuiwen/habittodoist/view/ProjectResultActivity.kt
@@ -0,0 +1,13 @@
+package com.example.yanghuiwen.habittodoist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yanghuiwen.habittodoist.R
+
+class ProjectResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_result)
+    }
+}
--- app/src/main/java/com/example/yanghuiwen/habittodoist/view/item_sample/TableProjectItem.kt
+++ app/src/main/java/com/example/yanghuiwen/habittodoist/view/item_sample/TableProjectItem.kt
@@ -1,12 +1,16 @@
 package com.example.yanghuiwen.habittodoist.view.item_sample
 
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.example.yanghuiwen.habittodoist.ItemDate
 import com.example.yanghuiwen.habittodoist.R
+import com.example.yanghuiwen.habittodoist.view.HabitResultActivity
+import com.example.yanghuiwen.habittodoist.view.ProjectResultActivity
 import java.util.ArrayList
 class TableProjectItem(context: Context, data: Map<String, ArrayList<ItemDate>>, toDoName :String) :  BaseAdapter() {
         private var datas =data
@@ -25,7 +29,7 @@ class TableProjectItem(context: Context, data: Map<String, ArrayList<ItemDate>>,
         inner class MyHolder(){
             lateinit var infoText : TextView
             lateinit var completionRate : TextView
-//            lateinit var mRelative : RelativeLayout
+            lateinit var mTableItem : RelativeLayout
         }
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -43,7 +47,7 @@ class TableProjectItem(context: Context, data: Map<String, ArrayList<ItemDate>>,
                 if (view != null) {
                     myHolder.infoText = view.findViewById(R.id.info_text)
                     myHolder.completionRate = view.findViewById(R.id.completion_rate)
-//                    myHolder.mRelative = view.findViewById(R.id.Realtive)
+                    myHolder.mTableItem = view.findViewById(R.id.tableItem)
                     view.tag = myHolder
                 }
 
@@ -56,7 +60,17 @@ class TableProjectItem(context: Context, data: Map<String, ArrayList<ItemDate>>,
 
                 myHolder.infoText.text = dataKeys[position]
                 myHolder.completionRate.text = "未完成：${dataValue[position].size}個事項"
-//            myHolder.mRelative.setBackgroundColor(datas.get(position).colorId)
+
+            myHolder.mTableItem.setOnClickListener {
+                var bundle= Bundle()
+                bundle.putString("name","")
+                bundle.putString("projectIndex","")
+
+
+                var intent = Intent(context, ProjectResultActivity::class.java)
+                intent.putExtra("bundle",bundle)
+                context.startActivity(intent)
+            }
 
             return view!!
         }
